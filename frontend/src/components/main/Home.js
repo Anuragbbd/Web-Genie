ef83801d547d1025a3f647861944d52317294bb7
Anuragbbd <anuragguru69@gmail.com>
2023-05-27 13:06:38 +0530

    ...

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='home-page'>
+    <div className='home-page bg-image'
+      style={{ backgroundImage: `url('/home page BG.jpg')` }}>
+'>
       <h1 className="text-center text-dark display-1 fw-bold p-4">
         Home Page
       </h1>
